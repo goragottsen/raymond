9540d9576566b5dbea06cc69a8921039dd9d7fa9
goragottsen <goragottsen@gmail.com>
2019-07-08 20:12:19 -0400

    Added some inline styling

--- src/App.js
+++ src/App.js
@@ -35,12 +35,23 @@ class App extends Component {
   }
 
   render() {
+    // Inline style - scoped to the component
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         {/* Don't use often to avoid frequent re-render. Use bind (below) instead! */}
-        <button onClick={() => this.switchNameHandler('Nikolai')}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Nikolai')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
