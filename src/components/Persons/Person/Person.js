68d17e338475d7278a91795aae271823ee2e3251
goragottsen <goragottsen@gmail.com>
2019-09-02 22:55:39 -0400

    Setting state correctly

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Aux from '../../../hoc/Aux'
+import withClass from '../../../hoc/withClass'
 import classes from './Person.css';
 
 class Person extends Component {
@@ -14,11 +15,11 @@ class Person extends Component {
                 <h4 onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</h4>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-                <p>{this.props.comment}</p>ß
+                <p>{this.props.comment}</p>
             </Aux>        
         )
     }
     
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/withClass';
 import Aux from '../hoc/Aux';
 import withClass from '../hoc/withClass';
 class App extends Component {
@@ -46,7 +45,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showNames: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   deletePersonHandler = (personIndex) => {
@@ -72,8 +72,15 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
-  }
+
+    //Best practice to update the state which depends on the old state
+    this.setState((prevState, props) => {
+      return { 
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1 
+      };
+    });
+  };
 
   toggleNamesHandler = () => {
     const doesShow = this.state.showNames;
